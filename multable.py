c24bfd472ba7de4bcff91329176f6574bf218e79
shravani <164602979+shravani2138@users.noreply.github.com>
2024-07-29 10:41:43 +0530

    Add files via upload

---
+++ multable.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Multiplication table (from 1 to 10) in Python
+
+
+# In[5]:
+
+
+num = 12
+num = int(input("Display multiplication table of? "))
+
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
+
+
+# In[ ]:
+
+
+
+
