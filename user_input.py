4733a5d4e3f726a633bcfa2b369b703456d7026b
shravani <164602979+shravani2138@users.noreply.github.com>
2024-07-31 19:32:16 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Prompting the user for input
+
+
+# In[5]:
+
+
+user_input = int(input())
+number_list = []
+ 
+for i in range(user_input):
+  number_list.append(int(input()))
+
+print("List of numbers:", number_list)
+
+
+# In[ ]:
+
+
+
+
