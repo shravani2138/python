f9b0d1738dc29814d9e3cba46ad4d77e328c818a
shravani <164602979+shravani2138@users.noreply.github.com>
2024-08-02 16:30:50 +0530

    Add files via upload

---
+++ positive_no.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Python program to print positive Numbers in given range 
+
+
+# In[3]:
+
+
+start, end = -4, 19
+for num in range(start, end + 1): 
+
+    if num >= 0: 
+        print(num, end=" ") 
+
+
+# In[ ]:
+
+
+
+
